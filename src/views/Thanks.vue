8e84a6c0db0eaac0532da88cb4027a0d57045ff3
Fernando <farriagadal94@gmail.com>
2023-03-27 22:19:15 -0300

    feat: added thanks page

--- src/views/Thanks.vue
+++ src/views/Thanks.vue
@@ -1,14 +1,57 @@
 <template>
-  <div>
-    <h1>Thanks</h1>
-    <p>
-      This is the Thanks page.
-    </p>
+  <div class="container thanks">
+    <div>
+      <h1>Thank You!</h1>
+      <p>You will get notified when iPhone <br>Photo Academy becomes available</p>
+      <GradientButton>Subscribe To Our Youtube Channel</GradientButton>
+    </div>
   </div>
 </template>
 
 <script>
+import GradientButton from '@/components/GradientButton.vue'
+
 export default {
-  name: 'ThanksView'
+  name: 'ThanksView',
+
+  components: {
+    GradientButton
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.thanks {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+
+
+  h1 {
+    font-family: 'Montserrat';
+    font-size: 32px;
+    font-weight: 400;
+    line-height: 48px;
+    text-align: center;
+    color: #EEECEC;
+    margin: 0;
+  }
+
+  p {
+    font-family: 'Montserrat';
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 26px;
+    text-align: center;
+    color: #EEECEC;
+    margin: 0;
+    margin-bottom: 16px;
+  }
+
+  button {
+    margin: auto;
+  }
+}
+
+</style>
