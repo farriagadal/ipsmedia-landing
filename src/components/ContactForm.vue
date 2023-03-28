ad074baa568c5245cac135b9e53417e30f4700b5
Fernando <farriagadal94@gmail.com>
2023-03-27 22:18:23 -0300

    feat: added contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,56 +1,124 @@
 <template>
-  <div>
-    <input type="text" placeholder="Enter your Email Address">
-    <button>Please Notify Me</button>
+  <div class="contact">
+    <div class="contact__input">
+      <label :class="{ active: isActive }">
+        <span v-if="!hasError">Enter your Email Address</span>
+        <span v-else class="error">Please enter a valid email</span>
+      </label>
+      <input
+        type="text"
+        :class="{ error: hasError }"
+        v-model="email"
+        @input="validateEmail"
+        @focus="inputActive = true"
+        @blur="inputActive = false"
+      />
+      <img src="~@/assets/images/send-icon.svg" alt="send" />
+    </div>
+    <GradientButton @click="submitForm">Please Notify Me</GradientButton>
   </div>
 </template>
 
 <script>
+import GradientButton from '@/components/GradientButton.vue';
+
 export default {
-  name: 'ContactForm'
-}
+  name: 'ContactForm',
+  components:{
+    GradientButton
+  },
+  data() {
+    return {
+      email: '',
+      hasError: false,
+      inputActive: false,
+    };
+  },
+  computed: {
+    isActive() {
+      return this.email.length > 0 || this.inputActive;
+    },
+  },
+  methods: {
+    validateEmail() {
+      const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+      if (this.email === '') {
+        this.hasError = false;
+      } else {
+        this.hasError = !emailPattern.test(this.email);
+      }
+    },
+    submitForm() {
+      this.validateEmail();
+      if (this.email !== '' && !this.hasError) {
+        this.$router.push('/thanks');
+      } else {
+        this.hasError = true;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-div {
+.contact {
   text-align: left;
   display: block;
 
-  input {
-    background: #171715;
-    border: 1px solid #3A3A3A;
-    border-radius: 6px;
-    height: 62px;
-    max-width: 423px;
+  &__input {
+    position: relative;
     width: 100%;
-    padding: 0 16px;
-    color: #EEECEC;
+    max-width: calc(423px);
 
-    &:focus {
-      outline: none;
-    }
-
-    &::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    label {
+      position: absolute;
+      top: 20px;
+      left: 17px;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 22px;
       color: #EEECEC;
+      transition: 0.2s;
+      pointer-events: none;
+
+      &.active {
+        font-size: 12px;
+        color: #3A3A3A;
+        top: 8px;
+      }
+
+      span.error {
+        color: #D41C3D;
+      }
     }
-  }
 
-  button {
-    display: block;
-    background: linear-gradient(135deg, #370C14 0%, #280C23 100%);
-    border-radius: 6px;
-    border: 1px solid #861b59;
-    padding: 16px 35px;
-    font-weight: 600;
-    font-size: 16px;
-    line-height: 16px;
-    margin-top: 16px;
-    color: #EEECEC;
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
+    input {
+      background: #171715;
+      border: 1px solid #3A3A3A;
+      border-radius: 6px;
+      max-height: 62px;
+      width: stretch;
+      padding: 25px 16px 15px 16px;
+      color: #EEECEC;
 
-    &:hover, &:active {
-      background: linear-gradient(135deg, #62051C 0%, #440C3A 100%);
+      &:focus {
+        outline: none;
+      }
+
+      &::placeholder {
+        color: #EEECEC;
+      }
+
+      &.error {
+        border-color: #D41C3D;
+      }
+    }
+    img {
+      position: absolute;
+      right: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+      cursor: pointer;
     }
   }
 }
---
+++ src/components/GradientButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <button @click="$emit('click')" class="gradient-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'GradientButton',
+};
+</script>
+
+<style scoped lang="scss">
+.gradient-button {
+  display: block;
+  position: relative;
+  border-radius: 6px;
+  border: 1px solid #861b59;
+  padding: 16px 35px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 16px;
+  margin-top: 16px;
+  color: #EEECEC;
+  cursor: pointer;
+  overflow: hidden;
+  background: none;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(135deg, #370C14 0%, #280C23 100%);
+    border-radius: 6px;
+    transition: opacity 0.2s ease-in-out;
+    z-index: -1;
+  }
+
+  &:hover::before,
+  &:active::before {
+    opacity: 0;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(135deg, #62051C 0%, #440C3A 100%);
+    border-radius: 6px;
+    z-index: -2;
+  }
+}
+</style>
